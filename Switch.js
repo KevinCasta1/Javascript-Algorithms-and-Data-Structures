b57c5c50e71552366e2974f8e2c7cc8be29f0b87
Kevin Castaneda <cascascas493@gmail.com>
2023-05-28 00:26:02 -0400

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,48 @@
+/*
+Replacing If Else Chains with Switch
+*/
+
+function chainToSwitch(val) {
+  let answer = "";
+
+  switch (val) {
+    case "bob":
+    answer = "Marley";
+    break;
+    case 42:
+    answer = "The Answer";
+    break;
+    case 1: 
+    answer = "There is no #1";
+    break;
+    case 99:
+    answer = "Missed me by this much!";
+    break;
+    case "John":
+    answer = "";
+    break;
+    case 156:
+    answer = "";
+    break;
+    default:
+    answer = "Ate Nine";
+  }
+  return answer;
+}
+
+chainToSwitch(7);
+
+/*
+if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+*/
+
